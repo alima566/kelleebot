fcf53f69947b1b4a1a63cb68db6cb6478b322009
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-25 14:49:03 -0700

    added channels for shoutout command

--- commands/multiple/mod/so.js
+++ commands/multiple/mod/so.js
@@ -1,9 +1,14 @@
 const fetch = require("node-fetch");
 
 const shoutouts = {
-  ramenbomber_: `Mmmmm... do you like Ramen? 🍜🍜🍜 If you do, then you should also check out <userToSo> at https://www.twitch.tv/<userToSo>! They were last seen playing <game>. PrideFloat PrideFloat`,
+  ramenbomber_:
+    "Mmmmm... do you like Ramen? 🍜🍜🍜 If you do, then you should also check out <userToSo> at https://www.twitch.tv/<userToSo>! They were last seen playing <game>. PrideFloat PrideFloat",
   "7squish":
     "Show some love and support for <userToSo> PrideRise PrideRise PrideRise They last played <game> at https://www.twitch.tv/<userToSo>",
+  mackthevoid:
+    "mackth2Fngrgun Look! It's <userToSo> at https://www.twitch.tv/<userToSo> !!! You should follow them and watch them play <game>. Give them some love for me~~~ mackth2Fngrgun",
+  bearyclairey:
+    "I would be beary thankful if you would check out <userToSo> at https://www.twitch.tv/<userToSo>! They were last playing <game>. TBAngel TBAngel",
 };
 
 module.exports = {
@@ -11,7 +16,7 @@ module.exports = {
   aliases: ["shoutout"],
   category: "Moderation",
   description: "Shouts a fellow streamer out.",
-  channel: ["ramenbomber_", "7squish"],
+  channel: ["ramenbomber_", "7squish", "mackthevoid", "bearyclairey"],
   isModOnly: true,
   execute: async ({ client, channel, args }) => {
     let userToSo = args[0].startsWith("@")
