bca0937efeae2cd51a44d90ac6abde58dbbad97c
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-25 15:54:21 -0700

    made function async

--- commands/mackthevoid/deaths.js
+++ commands/mackthevoid/deaths.js
@@ -7,12 +7,14 @@ module.exports = {
   description: "Tells you how many times Mack has died for a game.",
   cooldown: 15,
   channel: "mackthevoid",
-  execute: ({ client, channel }) => {
-    let game = (args.length === 0) ? await getGame(channel) : args.join(' ');
+  execute: async ({ client, channel }) => {
+    let game = args.length === 0 ? await getGame(channel) : args.join(" ");
     const deathCount = await getDeathCount(channel, game.toLowerCase());
     return client.say(
       channel,
-      `/me MacK has died a total of ${deathCount} times for ${game} ${deathCount === 0 ? 'PridePog' : 'kellee1Cry'}`
+      `/me MacK has died a total of ${deathCount} times for ${game} ${
+        deathCount === 0 ? "PridePog" : "kellee1Cry"
+      }`
     );
   },
 };
