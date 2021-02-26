52180b81d7648880b3cc4bc4c7650b9b40ad0206
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-25 21:40:01 -0700

    code refactor

--- events/raided.js
+++ events/raided.js
@@ -1,25 +1,15 @@
-// const ramenCommands = require("@root/ramen-commands");
-// const mackCommands = require("@root/mack-commands");
+const { shoutouts } = require("@utils/shoutouts");
 
 module.exports = (client, channel, username, viewers) => {
-  if (
-    channel.slice(1) === "mackthevoid" ||
-    channel.slice(1) === "ramenbomber_"
-  ) {
+  const channelName = channel.slice(1).toLowerCase();
+  if (channelName === "mackthevoid" || channelName === "ramenbomber_") {
     client.say(
       channel,
       `/me Incoming raid! Thank you @${username} for raiding the channel with ${viewers} viewer${
         viewers !== 1 ? "s" : ""
       }! Welcome raiders!`
     );
-    shoutouts(client, channel, username, viewers);
-  }
-};
 
-const shoutouts = (client, channel, username, viewers) => {
-  if (channel.slice(1).toLowerCase() === "mackthevoid") {
-    //mackCommands.shoutout(client, channel, null, username);
-  } else if (channel.slice(1).toLowerCase() === "ramenbomber_") {
-    //ramenCommands.shoutout(client, channel, null, username);
+    client.say(channel, `/me ${shoutouts[channelName]}`);
   }
 };
