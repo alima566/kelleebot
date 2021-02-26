ef4a8b0eb744ae62fba4e0cd8cbc1a417fe4cea7
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-25 20:25:35 -0700

    added links dbHelper

---
+++ utils/dbHelpers/links.js
@@ -0,0 +1,60 @@
+const linkSchema = require("@schemas/linkSchema");
+const { log } = require("@utils/utils");
+const linkCache = {};
+
+const getLink = async (channelName) => {
+  const cachedValue = linkCache[`${channelName}`];
+  if (cachedValue) {
+    console.log("FETCHING FROM CACHE");
+    return cachedValue;
+  }
+
+  let links = {};
+  console.log("FETCHING FROM DB");
+  try {
+    const result = await linkSchema.findOne({
+      channelName,
+    });
+    if (!result) {
+      return;
+    }
+
+    const { link } = result;
+    links = { link };
+
+    return links;
+  } catch (e) {
+    log("ERROR", "./utils/dbHelpers/links.js", e.message);
+  }
+};
+
+const setLink = async (channelName, userstate, link) => {
+  try {
+    const result = await linkSchema.findOneAndUpdate(
+      {
+        channelName,
+      },
+      {
+        $set: {
+          updatedBy: userstate.username,
+          link,
+        },
+      },
+      {
+        upsert: true,
+        new: true,
+      }
+    );
+    linkCache[`${channelName}`] = {
+      link: result.link,
+    };
+    return result;
+  } catch (e) {
+    log("ERROR", "./utils/dbHelpers/links.js", e.message);
+  }
+};
+
+module.exports = {
+  getLink,
+  setLink,
+};
