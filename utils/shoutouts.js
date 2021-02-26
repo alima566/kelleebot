3712a540f904b340de0d8dd5c806fbef8bbc52e3
Aaron Li-Mai <aaronmai@hotmail.com>
2021-02-25 21:32:06 -0700

    moved shoutouts to it's own file

---
+++ utils/shoutouts.js
@@ -0,0 +1,14 @@
+const shoutouts = {
+  ramenbomber_:
+    "Mmmmm... do you like Ramen? 🍜🍜🍜 If you do, then you should also check out <userToSo> at https://www.twitch.tv/<userToSo>! They were last seen playing <game>. PrideFloat PrideFloat",
+  "7squish":
+    "Show some love and support for <userToSo> PrideRise PrideRise PrideRise They last played <game> at https://www.twitch.tv/<userToSo>",
+  mackthevoid:
+    "mackth2Fngrgun Look! It's <userToSo> at https://www.twitch.tv/<userToSo> !!! You should follow them and watch them play <game>. Give them some love for me~~~ mackth2Fngrgun",
+  bearyclairey:
+    "I would be beary thankful if you would check out <userToSo> at https://www.twitch.tv/<userToSo>! They were last playing <game>. TBAngel TBAngel",
+};
+
+module.exports = {
+  shoutouts,
+};
